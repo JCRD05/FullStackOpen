b47cfa2915275aa145f0e5222d5cdf124ce4527c
Jose Carlos Ruiz Diaz <josecarlosruizdiaz05@gmail.com>
2026-05-11 08:03:07 -0600

    exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = ({course}) => {
+  return <h1>{course}</h1>
+}
+
+const Part = ({part, exercises}) => {
+  return <p>{part} {exercises}</p>
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {
+        parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises}></Part>)
+      }
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const sum = parts.reduce((sum, part) => sum += part.exercises, 0)
+  return <p>Number of exercises {sum}</p>
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
